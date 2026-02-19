243356801b949871ea7ec2e0657445eb400bee8a
DHARANEESHGEK-02 <dharaneeshgek1@gmail.com>
2026-02-19 12:27:41 +0530

    Initial commit - Student Rails CRUD completed

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative "boot"
+
+require "rails/all"
+require "logger"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module StudentManagement
+  class Application < Rails::Application
+    config.load_defaults 6.1
+  end
+end
